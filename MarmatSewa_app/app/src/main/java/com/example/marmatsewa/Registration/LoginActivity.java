0a04c0f848840309d7372cf5845a4a53fbea7047
sunil172 <sunil.shrestha172@gmail.com>
2020-12-10 14:47:12 +0545

    api dependencies installed

--- MarmatSewa_app/app/src/main/java/com/example/marmatsewa/Registration/LoginActivity.java
+++ MarmatSewa_app/app/src/main/java/com/example/marmatsewa/Registration/LoginActivity.java
@@ -3,14 +3,27 @@ package com.example.marmatsewa.Registration;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.marmatsewa.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private EditText edtEmail, edtPword;
+    private ImageView btnLogin;
+    private TextView btnSignup;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        edtEmail = findViewById(R.id.edtEmail);
+        edtPword = findViewById(R.id.edtPword);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnSignup = findViewById(R.id.btnSignup);
+
     }
 }
---
+++ MarmatSewa_app/app/src/main/java/com/example/marmatsewa/url/URL.java
@@ -0,0 +1,4 @@
+package com.example.marmatsewa.url;
+
+public class URL {
+}
