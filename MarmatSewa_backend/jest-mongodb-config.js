facc23db70e762d7fee36c80ca0e29b4e2313178
aashikmajhi <aashikmajhi@gmail.com>
2020-12-13 12:22:16 +0545

    setup has been done for testing endpoints

---
+++ MarmatSewa_backend/jest-mongodb-config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    mongodbMemoryServerOptions: {
+        instance: {
+            dbName: ''
+        },
+        binary: {
+            version: '4.0.2',
+            skipMD5: true
+        },
+
+        autostart: false
+    }
+}
