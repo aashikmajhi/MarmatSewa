3322e611e4000181d344cbaf631675012ea3d28e
niteshkatwal20 <nitesh.katwal20@gmail.com>
2020-12-15 22:46:43 +0545

    user login testing (unit testing and instrumental testing)

---
+++ MarmatSewa_app/app/src/androidTest/java/com/example/marmatsewa/UserLoginTest.java
@@ -0,0 +1,35 @@
+package com.example.marmatsewa;
+
+import androidx.test.espresso.matcher.ViewMatchers;
+import androidx.test.rule.ActivityTestRule;
+
+import com.example.marmatsewa.Registration.LoginActivity;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
+public class UserLoginTest {
+    @Rule
+public ActivityTestRule<LoginActivity>testRule=new ActivityTestRule<>(LoginActivity.class);
+    @Test
+    public void LoginTest() {
+        onView(ViewMatchers.withId(R.id.edtEmail))
+                .perform(typeText("nitesh.katwal20@gmail.com"))
+                .perform(closeSoftKeyboard());
+
+        onView(withId(R.id.edtPword))
+                .perform(typeText("Nit3sh@123"))
+                .perform(closeSoftKeyboard());
+
+        onView(withId(R.id.btnLogin))
+                .perform(click());
+
+    }
+}
+
