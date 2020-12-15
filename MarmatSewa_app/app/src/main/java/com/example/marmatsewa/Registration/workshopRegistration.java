236f2af81d36ed91bc10a05bb7d90287b209f563
SUSAN DUWADI <susanduwadi@SUSANs-MacBook-Pro.local>
2020-12-15 17:40:23 +0545

    UserRegistration form frontend

---
+++ MarmatSewa_app/app/src/main/java/com/example/marmatsewa/Registration/workshopRegistration.java
@@ -0,0 +1,16 @@
+package com.example.marmatsewa.Registration;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.marmatsewa.R;
+
+public class workshopRegistration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_workshop_registration);
+    }
+}
