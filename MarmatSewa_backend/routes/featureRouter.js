d5285af23c6aabba4e0cacb2e4b3262902cb1d4e
Rakesh Gyawali <41475122+rakesh-gyawali@users.noreply.github.com>
2020-12-17 19:16:47 +0545

    add UPDATE and DELETE feature

--- MarmatSewa_backend/routes/featureRouter.js
+++ MarmatSewa_backend/routes/featureRouter.js
@@ -18,8 +18,20 @@ router.route('/')
     .then(feature => {
         res.status(201).json(feature);
     }).catch(next);
-})
-.put((req, res, next) => {
+});
 
+router.route('/:feature_id')
+.put(auth.verifyUser, auth.verifyGarageOwner, (req, res, next) => {
+    const feature = { name, img } = req.body;
+    Feature.findByIdAndUpdate(req.params.feature_id, { $set: feature }, {new: true})
+    .then(updatedFeature => {
+        res.status(200).send(updatedFeature);
+    }).catch(next);
+})
+.delete(auth.verifyUser, auth.verifyGarageOwner, (req, res, next) => {
+    Feature.findByIdAndDelete(req.params.feature_id)
+    .then(deletedFeature => {
+        res.status(200).send(deletedFeature);
+    }).catch(next);
 })
 module.exports = router;
