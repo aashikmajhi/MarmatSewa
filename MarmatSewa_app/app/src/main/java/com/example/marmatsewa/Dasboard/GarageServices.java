d25a9371d32e1354d830a7fb76710feb163bb853
Sristi-Dumre <sristidumre123@gmail.com>
2020-12-19 20:11:58 +0545

    First commit

---
+++ MarmatSewa_app/app/src/main/java/com/example/marmatsewa/Dasboard/GarageServices.java
@@ -0,0 +1,14 @@
+package com.example.marmatsewa.Dasboard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GarageServices extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_garage_services);
+    }
+}
---
+++ MarmatSewa_app/app/src/main/java/com/example/marmatsewa/Dasboard/TwoWheelerServices.java
@@ -0,0 +1,16 @@
+package com.example.marmatsewa.Dasboard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.marmatsewa.R;
+
+public class GarageService2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_garage_service2);
+    }
+}
