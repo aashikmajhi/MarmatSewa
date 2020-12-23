2ec38a699e691c3b6fa76240a6ab5df275691eb5
Rakesh Gyawali <41475122+rakesh-gyawali@users.noreply.github.com>
2020-12-23 15:12:46 +0545

    organized folder and fixed put on feature

---
+++ MarmatSewa_backend/middlewares/authentication.js
@@ -0,0 +1,44 @@
+const jwt = require('jsonwebtoken');
+
+function verifyUser(req, res, next) {
+    let authHeader = req.headers.authorization;
+    if (!authHeader) {
+        let err = new Error('No authentication information!');
+		err.status = 401;
+        return next(err);
+    }
+
+    let token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.SECRET, (err, payload) => {
+        if (err) {
+            let err = new Error('Token could not be found!');
+            err.status = 401;
+            return next(err);
+        } else { 
+            req.user = payload;
+            next();
+        }
+    })
+};
+
+function verifyGarageOwner(req, res, next) {
+    if (req.user.role !== 'GARAGE_OWNER') {
+        let err = new Error('Forbidden');
+        err.status = 403;
+        return next(err);
+	}
+    next();
+}
+
+function verifyAdmin(req, res, next) {
+    if (req.user.role !== 'ADMIN') {
+        let err = new Error('Forbidden');
+        err.status = 403;
+        return next(err);
+	}
+    next();
+}
+
+module.exports = {
+    verifyUser, verifyGarageOwner, verifyAdmin
+}
--- MarmatSewa_backend/routes/featureRouter.js
+++ MarmatSewa_backend/routes/featureRouter.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const validators = require('../utils/garageValidation');
 const Feature = require('../models/Feature');
-const auth = require('./authentication');
+const auth = require('../middlewares/authentication');
 
 const router = express.Router();
 
@@ -22,10 +22,15 @@ router.route('/')
 
 router.route('/:feature_id')
 .put(auth.verifyUser, auth.verifyGarageOwner, (req, res, next) => {
+<<<<<<< Updated upstream
     const feature = { name, img } = req.body;
     Feature.findByIdAndUpdate(req.params.feature_id, { $set: feature }, {new: true})
+=======
+    const garage_feature = { feature, img } = req.body;
+    Feature.findByIdAndUpdate(req.params.feature_id, { $set: garage_feature }, {new: true})
+>>>>>>> Stashed changes
     .then(updatedFeature => {
-        res.status(200).send(updatedFeature);
+        res.status(201).send(updatedFeature);
     }).catch(next);
 })
 .delete(auth.verifyUser, auth.verifyGarageOwner, (req, res, next) => {
