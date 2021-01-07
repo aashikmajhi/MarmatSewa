7edd55b71c7fbd4acb3989308a4df623fc74d60b
Rakesh Gyawali <41475122+rakesh-gyawali@users.noreply.github.com>
2021-01-07 18:49:14 +0545

    debug has been done and added Request Schema

--- MarmatSewa_backend/routes/featureGarageOwnerRouter.js
+++ MarmatSewa_backend/routes/featureGarageOwnerRouter.js
@@ -8,19 +8,19 @@ router.route('/')
 .get(auth.verifyUser, (req, res, next ) => {
     Feature_GarageOwner.findById(req.user.id)
     .then(Feature_GarageOwner => {
-        res.status(201).json(Feature_GarageOwner);
+        res.status(200).json(Feature_GarageOwner);
     }).catch(next)
 })
 
-.post(auth.verifyUser, auth.verifyAdmin, (req, res, next) => {
-    Feature_GarageOwner.create({... req.body, garage: req.user.id})
+.post(auth.verifyUser, auth.verifyGarageOwner, (req, res, next) => {
+    Feature_GarageOwner.create({... req.body, garageOwner: req.user.id})
     .then(Feature_GarageOwner => {
         res.status(201).json(Feature_GarageOwner);
     }).catch(next);
 });
 
 router.route('/:FeatureGarageOwner_id')
-.delete(auth.verifyUser, auth.verifyAdmin, (req, res, next) => {
+.delete(auth.verifyUser, auth.verifyGarageOwner, (req, res, next) => {
     Feature_GarageOwner.findByIdAndDelete(req.params.FeatureGarageOwner_id)
     .then(deletedFeature_GarageOwner => {
         res.status(200).send(deletedFeature_GarageOwner);
