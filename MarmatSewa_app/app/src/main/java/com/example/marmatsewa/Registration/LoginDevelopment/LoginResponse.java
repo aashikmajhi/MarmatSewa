0912632a7216d7a53255026a9cdf4d862d9b3649
Sunil Shrestha <sunil.shrestha172@gmail.com>
2021-01-25 09:26:23 +0545

    request development

--- MarmatSewa_app/app/src/main/java/com/example/marmatsewa/Registration/LoginDevelopment/LoginResponse.java
+++ MarmatSewa_app/app/src/main/java/com/example/marmatsewa/Registration/LoginDevelopment/LoginResponse.java
@@ -5,11 +5,13 @@ public class LoginResponse {
     private String status;
     private String token;
     private String role;
+    private String user_id;
 
-    public LoginResponse(String status, String token, String role) {
+    public LoginResponse(String status, String token, String role, String user_id) {
         this.status = status;
         this.token = token;
         this.role = role;
+        this.user_id = user_id;
     }
 
     public String getStatus() {
@@ -34,5 +36,13 @@ public class LoginResponse {
 
     public void setRole(String role) {
         this.role = role;
+    }
+
+    public String getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(String user_id) {
+        this.user_id = user_id;
     }
 }
--- MarmatSewa_backend/routes/featureRouter.js
+++ MarmatSewa_backend/routes/featureRouter.js
@@ -7,68 +7,70 @@ const auth = require('../middlewares/authentication');
 
 const router = express.Router();
 
-router.route('/')
-.get(auth.verifyUser, (req, res, next ) => {
-    Feature.find()
-    .then(feature => {
-        res.status(200).json(feature);
-    }).catch(next)
+router
+	.route('/')
+	.get(auth.verifyUser, (req, res, next) => {
+		Feature.find()
+			.then((feature) => {
+				res.status(200).json(feature);
+			})
+			.catch(next);
+	})
+	.post(auth.verifyUser, auth.verifyGarageOwner, (req, res, next) => {
+		let { errors, isValid } = validators.FeatureInput(req.body);
+		if (!isValid) {
+			return res.status(400).json({
+				status: 'error',
+				message: errors
+			});
+		}
+		Feature.create({ ...req.body, garage: req.user.id });
 
-})
+		// .post(auth.verifyUser, auth.verifyAdmin, (req, res, next) => {
+		//     Feature.create({... req.body, admin: req.user.id})
 
-.post(auth.verifyUser, auth.verifyGarageOwner, (req, res, next) => {
-    let { errors, isValid } = validators.FeatureInput(req.body);
-    if (!isValid) {
-        return res.status(400).json({
-            status: 'error',
-            message: errors
-        });
-    }
-    Feature.create({... req.body, garage: req.user.id})
+		//     .then(feature => {
+		//         res.status(201).json(feature);
+		//     }).catch(next);
+	});
 
-// .post(auth.verifyUser, auth.verifyAdmin, (req, res, next) => {
-//     Feature.create({... req.body, admin: req.user.id})
+router
+	.route('/:feature_id')
+	.put(auth.verifyUser, auth.verifyGarageOwner, (req, res, next) => {
+		let { errors, isValid } = validators.FeatureInput(req.body);
+		if (!isValid) {
+			return res.status(400).json({
+				status: 'error',
+				message: errors
+			});
+		}
+		const feature = ({ name, img } = req.body);
+		Feature.findByIdAndUpdate(req.params.feature_id, { $set: feature }, { new: true });
+	})
+	.post(auth.verifyUser, auth.verifyAdmin, (req, res, next) => {
+		Feature.create({ ...req.body, admin: req.user.id })
+			.then((feature) => {
+				res.status(201).json(feature);
+			})
+			.catch(next);
+	});
 
-//     .then(feature => {
-//         res.status(201).json(feature);
-//     }).catch(next);
-// });
-
-router.route('/:feature_id')
-
-.put(auth.verifyUser, auth.verifyGarageOwner, (req, res, next) => {
-    let { errors, isValid } = validators.FeatureInput(req.body);
-    if (!isValid) {
-        return res.status(400).json({
-            status: 'error',
-            message: errors
-        });
-    }
-    const feature = { name, img } = req.body;
-    Feature.findByIdAndUpdate(req.params.feature_id, { $set: feature }, {new: true})
-
-.post(auth.verifyUser, auth.verifyAdmin, (req, res, next) => {
-    Feature.create({... req.body, admin: req.user.id})
-    .then(feature => {
-        res.status(201).json(feature);
-    }).catch(next);
-});
-
-router.route('/:feature_id')
-
-.put(auth.verifyUser, auth.verifyAdmin, (req, res, next) => {
-    const feat = req.body;
-    console.log(feat)
-    Feature.findByIdAndUpdate(req.params.feature_id, { $set: feat }, {new: true})
-
-    .then(updatedFeature => {
-        res.status(201).send(updatedFeature);
-    }).catch(next);
-})
-.delete(auth.verifyUser, auth.verifyAdmin, (req, res, next) => {
-    Feature.findByIdAndDelete(req.params.feature_id)
-    .then(deletedFeature => {
-        res.status(200).send(deletedFeature);
-    }).catch(next);
-})
+router
+	.route('/:feature_id')
+	.put(auth.verifyUser, auth.verifyAdmin, (req, res, next) => {
+		const feat = req.body;
+		console.log(feat);
+		Feature.findByIdAndUpdate(req.params.feature_id, { $set: feat }, { new: true })
+			.then((updatedFeature) => {
+				res.status(201).send(updatedFeature);
+			})
+			.catch(next);
+	})
+	.delete(auth.verifyUser, auth.verifyAdmin, (req, res, next) => {
+		Feature.findByIdAndDelete(req.params.feature_id)
+			.then((deletedFeature) => {
+				res.status(200).send(deletedFeature);
+			})
+			.catch(next);
+	});
 module.exports = router;
