4197548182e0f62b42c1257aae1caf6eb2acb183
SUSAN DUWADI <susanduwadi@SUSANs-MacBook-Pro.local>
2021-02-02 14:35:53 +0545

    updates on userprofile

--- MarmatSewa_app/app/src/main/java/com/example/marmatsewa/UserInterface/user_profile_view.java
+++ MarmatSewa_app/app/src/main/java/com/example/marmatsewa/UserInterface/user_profile_view.java
@@ -3,8 +3,15 @@ package com.example.marmatsewa.UserInterface;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.marmatsewa.R;
 
 public class user_profile_view extends AppCompatActivity {
+
+    ImageView btnBack, userLogout;
+    TextView tvUserFullName, tvUserEmail, tvUserPhone, tvUserAddress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
